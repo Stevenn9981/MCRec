004413fd1464866d29a48e36257a9b62f6c6a553
Stevenn9981 <stevenn9981@gmail.com>
2021-05-20 15:31:48 +0800

    modify

--- preprocess.py
+++ preprocess.py
@@ -21,32 +21,32 @@ random.seed(0)
 #     return sample_neg_items
 
 
-train_rate = 0.8
+# train_rate = 0.8
 
-train = []
+# train = []
 
-dict = collections.defaultdict(list)
+# dict = collections.defaultdict(list)
 # train_dict = collections.defaultdict(list)
-test_dict = collections.defaultdict(list)
+# test_dict = collections.defaultdict(list)
 
-
-data = []
-with open('data/yelp/yelp.train.rating', 'r') as infile:
-    for line in infile.readlines():
-        inter = [int(i) for i in line.strip().split('\t')]
-        data.append([inter[0], inter[1], 5])
-        dict[inter[0]].append(inter[1])
-        # train_dict[inter[0]].append(inter[1])
-
-with open('data/yelp/yelp.test.rating', 'r') as infile:
-    for line in infile.readlines():
-        inter = [int(i) for i in line.strip().split()]
-        user, item_ids = inter[0], inter[1:]
-        item_ids = list(set(item_ids))
-        for item in item_ids:
-            data.append([user, item, 5])
-            dict[user].append(item)
-            test_dict[inter[0]].append(str(item))
+#
+# data = []
+# with open('data/yelp/yelp.train.rating', 'r') as infile:
+#     for line in infile.readlines():
+#         inter = [int(i) for i in line.strip().split('\t')]
+#         data.append([inter[0], inter[1], 5])
+#         dict[inter[0]].append(inter[1])
+#         # train_dict[inter[0]].append(inter[1])
+#
+# with open('data/yelp/yelp.test.rating', 'r') as infile:
+#     for line in infile.readlines():
+#         inter = [int(i) for i in line.strip().split()]
+#         user, item_ids = inter[0], inter[1:]
+#         item_ids = list(set(item_ids))
+#         for item in item_ids:
+#             data.append([user, item, 5])
+#             dict[user].append(item)
+#             test_dict[inter[0]].append(str(item))
 
 # user_ids_batch = list(dict.keys())
 # neg_dict = collections.defaultdict(list)
@@ -63,15 +63,15 @@ with open('data/yelp/yelp.test.rating', 'r') as infile:
 
 # fw1 = open('data/ml-100k/yelp.train.rating', 'w')
 # fw2 = open('data/ml-100k/ml-100k.test.rating', 'w')
-fw3 = open('data/yelp/yelp.test.negative', 'w')
-
-
-for user in test_dict:
-    fw3.write('(' + str(user) + ',' + ','.join(test_dict[user]) + ')')
-    for i in range(1, 14285):
-        if i not in dict[user]:
-            fw3.write(' ' + str(i))
-    fw3.write('\n')
+# fw3 = open('data/yelp/yelp.test.negative', 'w')
+#
+#
+# for user in test_dict:
+#     fw3.write('(' + str(user) + ',' + ','.join(test_dict[user]) + ')')
+#     for i in range(1, 14285):
+#         if i not in dict[user]:
+#             fw3.write(' ' + str(i))
+#     fw3.write('\n')
 
 
 
@@ -81,3 +81,35 @@ for user in test_dict:
 #         trainfile.write('\t'.join(r) + '\n')
 #     for r in test:
 #         testfile.write('\t'.join(r) + '\n')
+
+
+data = []
+# with open('data/yelp/yelp.bpr.user_embedding', 'r') as infile:
+#     for line in infile.readlines():
+#         inter = [float(i) for i in line.strip().split('\t')]
+#         data.append([inter[0], inter[1], 5])
+#         dict[inter[0]].append(inter[1])
+#         # train_dict[inter[0]].append(inter[1])
+
+
+fw1 = open('data/yelp/yelp.bpr.item_embedding1', 'w')
+fw2 = open('data/yelp/yelp.bpr.user_embedding1', 'w')
+
+with open('data/yelp/yelp.bpr.item_embedding', 'r') as infile:
+    for line in infile.readlines():
+        inter = [float(i) for i in line.strip().split()]
+        id, embedding = int(inter[0]), inter[1:]
+        fw1.write(str(id + 1) + ' ')
+        for embed in embedding:
+            fw1.write(str(embed) + " ")
+        fw1.write('\n')
+
+
+with open('data/yelp/yelp.bpr.user_embedding', 'r') as infile:
+    for line in infile.readlines():
+        inter = [float(i) for i in line.strip().split()]
+        id, embedding = int(inter[0]), inter[1:]
+        fw2.write(str(id + 1) + ' ')
+        for embed in embedding:
+            fw2.write(str(embed) + " ")
+        fw2.write('\n')
